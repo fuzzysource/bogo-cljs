a0573e488d2962dd4f64a4ddd9ab5a92ce397508
fuzzysource <longdt90@gmail.com>
2014-05-09 03:15:06 +0100

    Enable typemode selection through main program arguments

--- src/bogo_clojure/bg_websocket.clj
+++ src/bogo_clojure/bg_websocket.clj
@@ -1,17 +1,20 @@
 (ns bogo-clojure.bg-websocket
   (:gen-class)
   (:require [bogo-clojure.core :as bogo]
+            [bogo-clojure.bg-typemode :refer :all]
             [clojure.string :as string])
   (:use org.httpkit.server))
 
+(def typemode (atom TELEX))
 
 (defn process!
   [channel data]
   (let [[old-string key] (string/split-lines data)
-        new-string (bogo/process-key old-string key)]
+        new-string (bogo/process-key old-string key TELEX)]
     (println old-string key new-string)
     (send! channel new-string)
     ))
+
 (defn handler [req]
   (with-channel req channel
     (on-close channel (fn [status]
@@ -24,4 +27,7 @@
 
 (defn -main
   [& args]
+  (if (not (empty? args))
+    (reset! typemode (eval (symbol "bogo-clojure.bg-typemode" (string/uppercase (string/join args)))))
+    true)
   (run-server handler {:port 8080}))
