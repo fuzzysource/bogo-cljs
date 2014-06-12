8cc9d38776ca038d682379901fe899c3c7bcc06d
fuzzysource <longdt90@gmail.com>
2014-06-12 23:17:16 +0100

    Process key at caret

--- src/bogo_cljs/bogo.cljs
+++ src/bogo_cljs/bogo.cljs
@@ -11,11 +11,23 @@
   []
   (.. js/document getSelection))
 
-(defn process-key
+(defn process-key-at-caret!
+  "When user do not select any text, the selection object .toString()
+  return an empty string. The new string combined from the text before
+  the caret and the entered key"
+  [selection key]
+  (let [word-before (last-word-before selection)
+        word-length (.-length word-before)
+        new-word (process_key word-before key)]
+    (do
+      (delete-word-before! selection word-before)
+      (insert-new-text! selection new-word))))
+
+(defn process-key!
   "Process the key event"
   [selection key]
   (if (no-text-selection selection)
-    (process-key-at-caret selection key)
-    (replace-selection-with-key selection key)))
+    (process-key-at-caret! selection key)
+    (replace-selection-with-key selection! key)))
 
 
